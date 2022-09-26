1b73436221d000a8df335b763410cecc3aeea59e
heebum99 <heebum9955@naver.com>
2022-09-26 17:06:12 +0900

    react 애플코딩 2장

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = '강남 고기 맛집';
+  let setting = {color : 'blue', fontSize : '30px'};
+
+  function 함수(){
+    return 100
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        {/* <div style={ {color : 'blue', fontSize : '30px'} }>개발 Blog</div> */}
+        <div style={setting}>개발 Blog</div>
+      </div>
+      {/* <img src={logo} />  */}
+      {/* 이미지 삽입할땐 import후 import명을 {}안에 넣어주기 */}
+      <h4>{ posts }</h4>
+      {/* <h4>{ 함수() }</h4>  */}
+       {/* {}안에 변수명, 함수 등을 통해서 데이터 바인딩 쉽게 가능  */}
     </div>
   );
 }
